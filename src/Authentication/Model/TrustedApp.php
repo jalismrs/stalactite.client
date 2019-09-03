4230d9a2b0c94049fd59072c558617be4f099919
hunomina <hugo.f972@gmail.com>
2019-09-03 17:57:30 +0200

    Add TrustedApp API model

---
+++ src/Authentication/Model/TrustedApp.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace jalismrs\Stalactite\Client\Authentication\Model;
+
+class TrustedApp
+{
+    /** @var string $uid */
+    private $uid;
+
+    /** @var string $name */
+    private $name;
+
+    /** @var string $googleOAuthClientId */
+    private $googleOAuthClientId;
+
+    /** @var string $authToken */
+    private $authToken;
+
+    /** @var string $resetToken */
+    private $resetToken;
+
+    /**
+     * @return string
+     */
+    public function getUid(): string
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param string $uid
+     * @return TrustedApp
+     */
+    public function setUid(string $uid): TrustedApp
+    {
+        $this->uid = $uid;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return TrustedApp
+     */
+    public function setName(string $name): TrustedApp
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleOAuthClientId(): string
+    {
+        return $this->googleOAuthClientId;
+    }
+
+    /**
+     * @param string $googleOAuthClientId
+     * @return TrustedApp
+     */
+    public function setGoogleOAuthClientId(string $googleOAuthClientId): TrustedApp
+    {
+        $this->googleOAuthClientId = $googleOAuthClientId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthToken(): string
+    {
+        return $this->authToken;
+    }
+
+    /**
+     * @param string $authToken
+     * @return TrustedApp
+     */
+    public function setAuthToken(string $authToken): TrustedApp
+    {
+        $this->authToken = $authToken;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResetToken(): string
+    {
+        return $this->resetToken;
+    }
+
+    /**
+     * @param string $resetToken
+     * @return TrustedApp
+     */
+    public function setResetToken(string $resetToken): TrustedApp
+    {
+        $this->resetToken = $resetToken;
+        return $this;
+    }
+}
