0d6355c56b61ff54bfc45ec42c3375cf60563db3
hunomina <hugo.f972@gmail.com>
2019-09-04 17:03:03 +0200

    Implement Data Management API models

--- src/DataManagement/Model/CertificationType.php
+++ src/DataManagement/Model/CertificationType.php
@@ -2,7 +2,39 @@
 
 namespace jalismrs\Stalactite\Client\DataManagement\Model;
 
-class CertificationType
-{
+use jalismrs\Stalactite\Client\AbstractModel;
 
+class CertificationType extends AbstractModel
+{
+    /** @var null|string $name */
+    private $name;
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return CertificationType
+     */
+    public function setName(?string $name): CertificationType
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function asArray(): array
+    {
+        return [
+            'uid' => $this->uid,
+            'name' => $this->name
+        ];
+    }
 }
