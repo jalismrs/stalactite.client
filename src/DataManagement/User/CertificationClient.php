477210598f5d8304b77fb1b6b4ef82f0ae24215d
hunomina <hugo.f972@gmail.com>
2019-09-05 16:51:18 +0200

    Implement User\CertificationClient + tests

--- src/DataManagement/User/CertificationClient.php
+++ src/DataManagement/User/CertificationClient.php
@@ -2,9 +2,17 @@
 
 namespace jalismrs\Stalactite\Client\DataManagement\User;
 
+use hunomina\Validator\Json\Exception\InvalidDataException;
+use hunomina\Validator\Json\Exception\InvalidDataTypeException;
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use hunomina\Validator\Json\Rule\JsonRule;
+use hunomina\Validator\Json\Schema\JsonSchema;
 use jalismrs\Stalactite\Client\AbstractClient;
+use jalismrs\Stalactite\Client\ClientException;
 use jalismrs\Stalactite\Client\DataManagement\Model\CertificationGraduation;
+use jalismrs\Stalactite\Client\DataManagement\Model\CertificationType;
 use jalismrs\Stalactite\Client\DataManagement\Model\User;
+use jalismrs\Stalactite\Client\DataManagement\Schema;
 
 class CertificationClient extends AbstractClient
 {
@@ -14,10 +22,23 @@ class CertificationClient extends AbstractClient
      * @param User $user
      * @param string $jwt
      * @return array
+     * @throws InvalidSchemaException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws ClientException
      */
     public function getAll(User $user, string $jwt): array
     {
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+            'certifications' => ['type' => JsonRule::LIST_TYPE, 'schema' => Schema::CERTIFICATION_GRADUATION]
+        ]);
 
+        return $this->request('GET', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX, [
+            'headers' => ['X-API-TOKEN' => $jwt]
+        ], $schema);
     }
 
     /**
@@ -25,10 +46,34 @@ class CertificationClient extends AbstractClient
      * @param CertificationGraduation $certificationGraduation
      * @param string $jwt
      * @return array
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
      */
-    public function addCertificationGraduation(User $user, CertificationGraduation $certificationGraduation, string $jwt): array
+    public function addCertification(User $user, CertificationGraduation $certificationGraduation, string $jwt): array
     {
+        if (!($certificationGraduation->getType() instanceof CertificationType)) {
+            throw new ClientException('Certification Graduation type must be a Certification Type');
+        }
 
+        $body = [
+            'date' => $certificationGraduation->getDate(),
+            'type' => [
+                'uid' => $certificationGraduation->getType()->getUid()
+            ]
+        ];
+
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+        ]);
+
+        return $this->request('POST', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX, [
+            'headers' => ['X-API-TOKEN' => $jwt],
+            'json' => $body
+        ], $schema);
     }
 
     /**
@@ -36,9 +81,21 @@ class CertificationClient extends AbstractClient
      * @param CertificationGraduation $certificationGraduation
      * @param string $jwt
      * @return array
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
      */
-    public function deleteCertificationGraduation(User $user, CertificationGraduation $certificationGraduation, string $jwt): array
+    public function removeCertification(User $user, CertificationGraduation $certificationGraduation, string $jwt): array
     {
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true]
+        ]);
 
+        return $this->request('DELETE', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX . '/' . $certificationGraduation->getUid(), [
+            'headers' => ['X-API-TOKEN' => $jwt]
+        ], $schema);
     }
 }
