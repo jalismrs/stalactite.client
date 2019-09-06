0d4a3a86e32410ea384cbe916b809c366d352344
hunomina <hugo.f972@gmail.com>
2019-09-06 10:03:47 +0200

    Implement CertificationTypeClient + tests

--- src/DataManagement/CertificationTypeClient.php
+++ src/DataManagement/CertificationTypeClient.php
@@ -2,7 +2,13 @@
 
 namespace jalismrs\Stalactite\Client\DataManagement;
 
+use hunomina\Validator\Json\Exception\InvalidDataException;
+use hunomina\Validator\Json\Exception\InvalidDataTypeException;
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use hunomina\Validator\Json\Rule\JsonRule;
+use hunomina\Validator\Json\Schema\JsonSchema;
 use jalismrs\Stalactite\Client\AbstractClient;
+use jalismrs\Stalactite\Client\ClientException;
 use jalismrs\Stalactite\Client\DataManagement\Model\CertificationType;
 
 class CertificationTypeClient extends AbstractClient
@@ -12,49 +18,120 @@ class CertificationTypeClient extends AbstractClient
     /**
      * @param string $jwt
      * @return array
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     * @throws ClientException
      */
     public function getAll(string $jwt): array
     {
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+            'certificationTypes' => ['type' => JsonRule::LIST_TYPE, 'schema' => Schema::CERTIFICATION_TYPE]
+        ]);
 
+        return $this->request('GET', $this->apiHost . self::API_URL_PREFIX, [
+            'headers' => ['X-API-TOKEN' => $jwt]
+        ], $schema);
     }
 
     /**
      * @param CertificationType $certificationType
      * @param string $jwt
      * @return array
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
      */
     public function get(CertificationType $certificationType, string $jwt): array
     {
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+            'certificationType' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => Schema::CERTIFICATION_TYPE]
+        ]);
 
+        return $this->request('GET', $this->apiHost . self::API_URL_PREFIX . '/' . $certificationType->getUid(), [
+            'headers' => ['X-API-TOKEN' => $jwt]
+        ], $schema);
     }
 
     /**
      * @param CertificationType $certificationType
      * @param string $jwt
      * @return array
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
      */
     public function create(CertificationType $certificationType, string $jwt): array
     {
+        $body = [
+            'name' => $certificationType->getName()
+        ];
 
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+            'certificationType' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => Schema::CERTIFICATION_TYPE]
+        ]);
+
+        return $this->request('POST', $this->apiHost . self::API_URL_PREFIX, [
+            'headers' => ['X-API-TOKEN' => $jwt],
+            'json' => $body
+        ], $schema);
     }
 
     /**
      * @param CertificationType $certificationType
      * @param string $jwt
      * @return array
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
      */
     public function update(CertificationType $certificationType, string $jwt): array
     {
+        $body = ['name' => $certificationType->getName()];
 
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true]
+        ]);
+
+        return $this->request('PUT', $this->apiHost . self::API_URL_PREFIX . '/' . $certificationType->getUid(), [
+            'headers' => ['X-API-TOKEN' => $jwt],
+            'json' => $body
+        ], $schema);
     }
 
     /**
      * @param CertificationType $certificationType
      * @param string $jwt
      * @return array
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
      */
     public function delete(CertificationType $certificationType, string $jwt): array
     {
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true]
+        ]);
 
+        return $this->request('DELETE', $this->apiHost . self::API_URL_PREFIX . '/' . $certificationType->getUid(), [
+            'headers' => ['X-API-TOKEN' => $jwt]
+        ], $schema);
     }
 }
---
+++ tests/DataManagement/CertificationTypeClientTest.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace jalismrs\Stalactite\Client\Test;
+
+use hunomina\Validator\Json\Exception\InvalidDataException;
+use hunomina\Validator\Json\Exception\InvalidDataTypeException;
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use jalismrs\Stalactite\Client\ClientException;
+use jalismrs\Stalactite\Client\DataManagement\CertificationTypeClient;
+use jalismrs\Stalactite\Client\DataManagement\Model\CertificationType;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+class CertificationTypeClientTest extends TestCase
+{
+    /**
+     * @return CertificationType
+     */
+    private static function getTestableCertificationType(): CertificationType
+    {
+        $type = new CertificationType();
+        $type->setName('azerty')->setUid('azertyuiop');
+
+        return $type;
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGetAll(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'certificationTypes' => [self::getTestableCertificationType()->asArray()]
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->getAll('fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseGetAll(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'certificationTypes' => self::getTestableCertificationType()->asArray() // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->getAll('fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGet(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'certificationType' => self::getTestableCertificationType()->asArray()
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->get(self::getTestableCertificationType(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseGet(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'certificationType' => [] // invalid certification type
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->get(self::getTestableCertificationType(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testCreate(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'certificationType' => self::getTestableCertificationType()->asArray()
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->create(self::getTestableCertificationType(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseCreate(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'certificationType' => [] // invalid certification type
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->create(self::getTestableCertificationType(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testUpdate(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->update(self::getTestableCertificationType(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseUpdate(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => false // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->update(self::getTestableCertificationType(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testDelete(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->delete(self::getTestableCertificationType(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseDelete(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => false /// invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new CertificationTypeClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->delete(self::getTestableCertificationType(), 'fake user jwt');
+    }
+}
