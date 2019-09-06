57ed6819100062ba24cdd8ddf58cae6e54498ae2
hunomina <hugo.f972@gmail.com>
2019-09-06 11:42:39 +0200

    Implement PostClient + tests

---
+++ tests/DataManagement/PostClientTest.php
@@ -0,0 +1,327 @@
+<?php
+
+namespace jalismrs\Stalactite\Client\Test;
+
+use hunomina\Validator\Json\Exception\InvalidDataException;
+use hunomina\Validator\Json\Exception\InvalidDataTypeException;
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use jalismrs\Stalactite\Client\ClientException;
+use jalismrs\Stalactite\Client\DataManagement\Model\Post;
+use jalismrs\Stalactite\Client\DataManagement\Model\User;
+use jalismrs\Stalactite\Client\DataManagement\PostClient;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+class PostClientTest extends TestCase
+{
+    /**
+     * @return User
+     */
+    private static function getTestableUser(): User
+    {
+        $user = new User();
+        $user->setFirstName('azerty')
+            ->setLastName('uiop')
+            ->setGender('male')
+            ->setEmail('goodMorning@hello.hi')
+            ->setPrivilege('user')
+            ->setBirthday('2000-01-01')
+            ->setUid('azertyuiop');
+
+        return $user;
+    }
+
+    /**
+     * @return Post
+     */
+    private static function getTestablePost(): Post
+    {
+        $post = new Post();
+        $post->setName('azerty')
+            ->setRank(1)
+            ->setPrivilege('user')
+            ->setShortName('aze')
+            ->setUid('azertyuiop');
+
+        return $post;
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGetAll(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'posts' => [self::getTestablePost()->asArray()]
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->getAll('fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseGetAll(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'posts' => self::getTestablePost()->asArray() // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->getAll('fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGet(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'post' => self::getTestablePost()->asArray()
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->get(self::getTestablePost(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseGet(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'post' => [] // invalid post
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->get(self::getTestablePost(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGetUsers(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'users' => [self::getTestableUser()->asArray()]
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->getUsers(self::getTestablePost(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseGetUsers(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'users' => null // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->getUsers(self::getTestablePost(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testCreate(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'post' => self::getTestablePost()->asArray()
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->create(self::getTestablePost(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseCreate(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'post' => [] // invalid post
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->create(self::getTestablePost(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testUpdate(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->update(self::getTestablePost(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseUpdate(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => false // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->update(self::getTestablePost(), 'fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testDelete(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $this->assertIsArray($mockAPIClient->delete(self::getTestablePost(), 'fake user jwt'));
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseDelete(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => false // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new PostClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->delete(self::getTestablePost(), 'fake user jwt');
+    }
+}
