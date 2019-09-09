dfc9128d3af7b0b7626d018165a19aa1ca48e3c6
hunomina <hugo.f972@gmail.com>
2019-09-09 11:29:34 +0200

    Refactor DataManagement\User\PhoneLineClient + tests to use the response wrapper

--- src/DataManagement/User/PhoneLineClient.php
+++ src/DataManagement/User/PhoneLineClient.php
@@ -9,10 +9,12 @@ use hunomina\Validator\Json\Rule\JsonRule;
 use hunomina\Validator\Json\Schema\JsonSchema;
 use jalismrs\Stalactite\Client\AbstractClient;
 use jalismrs\Stalactite\Client\ClientException;
+use jalismrs\Stalactite\Client\DataManagement\Model\ModelFactory;
 use jalismrs\Stalactite\Client\DataManagement\Model\PhoneLine;
 use jalismrs\Stalactite\Client\DataManagement\Model\PhoneType;
 use jalismrs\Stalactite\Client\DataManagement\Model\User;
 use jalismrs\Stalactite\Client\DataManagement\Schema;
+use jalismrs\Stalactite\Client\Response;
 
 class PhoneLineClient extends AbstractClient
 {
@@ -21,13 +23,13 @@ class PhoneLineClient extends AbstractClient
     /**
      * @param User $user
      * @param string $jwt
-     * @return array
+     * @return Response
      * @throws ClientException
      * @throws InvalidDataException
      * @throws InvalidDataTypeException
      * @throws InvalidSchemaException
      */
-    public function getAll(User $user, string $jwt): array
+    public function getAll(User $user, string $jwt): Response
     {
         $schema = new JsonSchema();
         $schema->setSchema([
@@ -36,22 +38,34 @@ class PhoneLineClient extends AbstractClient
             'phoneLines' => ['type' => JsonRule::LIST_TYPE, 'schema' => Schema::PHONE_LINE]
         ]);
 
-        return $this->request('GET', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX, [
+        $r = $this->request('GET', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX, [
             'headers' => ['X-API-TOKEN' => $jwt]
         ], $schema);
+
+        $phoneLines = [];
+        foreach ($r['phoneLines'] as $phoneLine) {
+            $phoneLines[] = ModelFactory::createPhoneLine($phoneLine);
+        }
+
+        $response = new Response();
+        $response->setSuccess($r['success'])->setError($r['error'])->setData([
+            'phoneLines' => $phoneLines
+        ]);
+
+        return $response;
     }
 
     /**
      * @param User $user
      * @param PhoneLine $phoneLine
      * @param string $jwt
-     * @return array
+     * @return Response
      * @throws ClientException
      * @throws InvalidDataException
      * @throws InvalidDataTypeException
      * @throws InvalidSchemaException
      */
-    public function addPhoneLine(User $user, PhoneLine $phoneLine, string $jwt): array
+    public function addPhoneLine(User $user, PhoneLine $phoneLine, string $jwt): Response
     {
         if (!($phoneLine->getType() instanceof PhoneType)) {
             throw new ClientException('Phone Line type must be a Phone Type', ClientException::INVALID_PARAMETER_PASSED_TO_CLIENT);
@@ -70,23 +84,28 @@ class PhoneLineClient extends AbstractClient
             'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
         ]);
 
-        return $this->request('POST', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX, [
+        $r = $this->request('POST', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX, [
             'headers' => ['X-API-TOKEN' => $jwt],
             'json' => $body
         ], $schema);
+
+        $response = new Response();
+        $response->setSuccess($r['success'])->setError($r['error']);
+
+        return $response;
     }
 
     /**
      * @param User $user
      * @param PhoneLine $phoneLine
      * @param string $jwt
-     * @return array
+     * @return Response
      * @throws ClientException
      * @throws InvalidDataException
      * @throws InvalidDataTypeException
      * @throws InvalidSchemaException
      */
-    public function removePhoneLine(User $user, PhoneLine $phoneLine, string $jwt): array
+    public function removePhoneLine(User $user, PhoneLine $phoneLine, string $jwt): Response
     {
         $schema = new JsonSchema();
         $schema->setSchema([
@@ -94,8 +113,13 @@ class PhoneLineClient extends AbstractClient
             'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true]
         ]);
 
-        return $this->request('DELETE', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX . '/' . $phoneLine->getUid(), [
+        $r = $this->request('DELETE', $this->apiHost . UserClient::API_URL_PREFIX . '/' . $user->getUid() . self::API_URL_PREFIX . '/' . $phoneLine->getUid(), [
             'headers' => ['X-API-TOKEN' => $jwt]
         ], $schema);
+
+        $response = new Response();
+        $response->setSuccess($r['success'])->setError($r['error']);
+
+        return $response;
     }
 }
