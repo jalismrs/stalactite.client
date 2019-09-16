d3a90c3b42231eb051c48e0a7c3334f898b6f720
hunomina <hugo.f972@gmail.com>
2019-09-16 11:52:58 +0200

    Implement DataManagement\AuthToken\UserClient + tests

--- src/DataManagement/AuthToken/UserClient.php
+++ src/DataManagement/AuthToken/UserClient.php
@@ -9,47 +9,114 @@ use hunomina\Validator\Json\Rule\JsonRule;
 use hunomina\Validator\Json\Schema\JsonSchema;
 use jalismrs\Stalactite\Client\AbstractClient;
 use jalismrs\Stalactite\Client\ClientException;
-use jalismrs\Stalactite\Client\DataManagement\Model\User;
+use jalismrs\Stalactite\Client\DataManagement\Model\ModelFactory;
+use jalismrs\Stalactite\Client\DataManagement\Schema;
 use jalismrs\Stalactite\Client\Response;
 
 class UserClient extends AbstractClient
 {
+    public const API_URL_PREFIX = AuthTokenClient::API_URL_PREFIX . '/users';
+
     /**
      * @param string $apiAuthToken
-     * @param User $user
      * @return Response
+     * @throws ClientException
      * @throws InvalidDataException
      * @throws InvalidDataTypeException
      * @throws InvalidSchemaException
-     * @throws ClientException
      */
-    public function getByEmailAndGoogleId(User $user, string $apiAuthToken): Response
+    public function getAll(string $apiAuthToken): Response
     {
         $jwt = AuthTokenClient::generateJwt($apiAuthToken, $this->userAgent);
-        $body = [
-            'email' => $user->getEmail(),
-            'googleId' => $user->getGoogleId()
-        ];
 
         $schema = new JsonSchema();
         $schema->setSchema([
             'success' => ['type' => JsonRule::BOOLEAN_TYPE],
             'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
-            'user' => ['type' => JsonRule::OBJECT_TYPE, 'null' => true, 'schema' => [
-                'uid' => ['type' => JsonRule::STRING_TYPE],
-                'type' => ['type' => JsonRule::STRING_TYPE, 'enum' => ['user', 'customer']],
-                'privilege' => ['type' => JsonRule::STRING_TYPE]
-            ]]
+            'users' => ['type' => JsonRule::LIST_TYPE, 'schema' => Schema::USER]
         ]);
 
-        $r = $this->request('POST', $this->apiHost . AuthTokenClient::API_URL_PREFIX . '/user', [
+        $r = $this->request('GET', $this->apiHost . self::API_URL_PREFIX, [
+            'headers' => ['X-API-TOKEN' => (string)$jwt]
+        ], $schema);
+
+        $users = [];
+        foreach ($r['users'] as $user) {
+            $users[] = ModelFactory::createUser($user);
+        }
+
+        $response = new Response();
+        $response->setSuccess($r['success'])->setError($r['error'])->setData([
+            'users' => $users
+        ]);
+
+        return $response;
+    }
+
+    /**
+     * @param string $email
+     * @param string $googleId
+     * @param string $apiAuthToken
+     * @return Response
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function getByEmailAndGoogleId(string $email, string $googleId, string $apiAuthToken): Response
+    {
+        $jwt = AuthTokenClient::generateJwt($apiAuthToken, $this->userAgent);
+
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+            'user' => ['type' => JsonRule::OBJECT_TYPE, 'null' => true, 'schema' => Schema::USER]
+        ]);
+
+        $r = $this->request('GET', $this->apiHost . self::API_URL_PREFIX, [
             'headers' => ['X-API-TOKEN' => (string)$jwt],
-            'json' => $body
+            'query' => [
+                'email' => $email,
+                'googleId' => $googleId
+            ]
         ], $schema);
 
         $response = new Response();
         $response->setSuccess($r['success'])->setError($r['error'])->setData([
-            'user' => $r['user']
+            'user' => $r['user'] ? ModelFactory::createUser($r['user']) : null
+        ]);
+
+        return $response;
+    }
+
+    /**
+     * @param string $uid
+     * @param string $apiAuthToken
+     * @return Response
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function get(string $uid, string $apiAuthToken): Response
+    {
+        $jwt = AuthTokenClient::generateJwt($apiAuthToken, $this->userAgent);
+
+        $schema = new JsonSchema();
+        $schema->setSchema([
+            'success' => ['type' => JsonRule::BOOLEAN_TYPE],
+            'error' => ['type' => JsonRule::STRING_TYPE, 'null' => true],
+            'user' => ['type' => JsonRule::OBJECT_TYPE, 'null' => true, 'schema' => Schema::USER]
+        ]);
+
+        $r = $this->request('GET', $this->apiHost . self::API_URL_PREFIX . '/' . $uid, [
+            'headers' => ['X-API-TOKEN' => (string)$jwt]
+        ], $schema);
+
+        $response = new Response();
+        $response->setSuccess($r['success'])->setError($r['error'])->setData([
+            'user' => $r['user'] ? ModelFactory::createUser($r['user']) : null
         ]);
 
         return $response;
--- tests/DataManagement/AuthToken/UserClientTest.php
+++ tests/DataManagement/AuthToken/UserClientTest.php
@@ -8,12 +8,113 @@ use hunomina\Validator\Json\Exception\InvalidSchemaException;
 use jalismrs\Stalactite\Client\ClientException;
 use jalismrs\Stalactite\Client\DataManagement\AuthToken\UserClient;
 use jalismrs\Stalactite\Client\DataManagement\Model\User;
+use jalismrs\Stalactite\Client\Test\DataManagement\ModelFactory;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
 
 class UserClientTest extends TestCase
 {
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGetAll(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'users' => [ModelFactory::getTestableUser()->asArray()]
+            ]))
+        ]);
+
+        $mockAPIClient = new UserClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $response = $mockAPIClient->getAll('fake user jwt');
+        $this->assertTrue($response->success());
+        $this->assertNull($response->getError());
+        $this->assertContainsOnlyInstancesOf(User::class, $response->getData()['users']);
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseGetAll(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'users' => null // invalid type
+            ]))
+        ]);
+
+        $mockAPIClient = new UserClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->getAll('fake user jwt');
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testGet(): void
+    {
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'user' => ModelFactory::getTestableUser()->asArray()
+            ]))
+        ]);
+
+        $mockAPIClient = new UserClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $response = $mockAPIClient->get('fake user uid', 'fake user jwt');
+        $this->assertTrue($response->success());
+        $this->assertNull($response->getError());
+        $this->assertInstanceOf(User::class, $response->getData()['user']);
+    }
+
+    /**
+     * @throws ClientException
+     * @throws InvalidDataException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseGet(): void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+
+        $mockHttpClient = new MockHttpClient([
+            new MockResponse(json_encode([
+                'success' => true,
+                'error' => null,
+                'user' => [] // invalid user
+            ]))
+        ]);
+
+        $mockAPIClient = new UserClient('http://fakeClient');
+        $mockAPIClient->setHttpClient($mockHttpClient);
+
+        $mockAPIClient->get('fake user uid', 'fake user jwt');
+    }
+
     /**
      * @throws InvalidDataException
      * @throws InvalidDataTypeException
@@ -26,27 +127,17 @@ class UserClientTest extends TestCase
             new MockResponse(json_encode([
                 'success' => true,
                 'error' => null,
-                'user' => [
-                    'uid' => 'azertyuiop',
-                    'type' => 'user',
-                    'privilege' => 'user'
-                ]
+                'user' => ModelFactory::getTestableUser()->asArray()
             ]))
         ]);
 
         $mockAPIClient = new UserClient('http://fakeClient');
         $mockAPIClient->setHttpClient($mockHttpClient);
 
-        $user = new User();
-        $user->setEmail('goodmorning@hello.hi')->setGoogleId('0123456789');
-
-        $response = $mockAPIClient->getByEmailAndGoogleId($user, 'fake user jwt');
+        $response = $mockAPIClient->getByEmailAndGoogleId('goodmorning@hello.hi', '0123456789', 'fake user jwt');
         $this->assertTrue($response->success());
         $this->assertNull($response->getError());
-        $this->assertTrue(isset($response->getData()['user']));
-        $this->assertTrue(isset($response->getData()['user']['uid']));
-        $this->assertTrue(isset($response->getData()['user']['type']));
-        $this->assertTrue(isset($response->getData()['user']['privilege']));
+        $this->assertInstanceOf(User::class, $response->getData()['user']);
     }
 
     /**
@@ -71,9 +162,6 @@ class UserClientTest extends TestCase
         $mockAPIClient = new UserClient('http://fakeClient');
         $mockAPIClient->setHttpClient($mockHttpClient);
 
-        $user = new User();
-        $user->setEmail('goodmorning@hello.hi')->setGoogleId('0123456789');
-
-        $mockAPIClient->getByEmailAndGoogleId($user, 'fake user jwt');
+        $mockAPIClient->getByEmailAndGoogleId('goodmorning@hello.hi', '0123456789', 'fake user jwt');
     }
 }
