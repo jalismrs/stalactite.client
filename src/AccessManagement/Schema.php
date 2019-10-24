e0b7aff6bce5c3ee88a510d2149c63f140d2a808
hunomina <hugo.f972@gmail.com>
2019-10-24 17:19:13 +0200

    Add DomainUserRelation schema

--- src/AccessManagement/Schema.php
+++ src/AccessManagement/Schema.php
@@ -3,11 +3,18 @@
 namespace jalismrs\Stalactite\Client\AccessManagement;
 
 use hunomina\Validator\Json\Rule\JsonRule;
+use jalismrs\Stalactite\Client\DataManagement\Schema as DataManagementSchema;
 
 abstract class Schema
 {
     public const ACCESS_CLEARANCE = [
         'accessGranted' => ['type' => JsonRule::BOOLEAN_TYPE],
         'accessType' => ['type' => JsonRule::STRING_TYPE, 'null' => true, 'enum' => ['user', 'admin']]
+    ];
+
+    public const DOMAIN_USER_RELATION = [
+        'uid' => ['type' => JsonRule::STRING_TYPE],
+        'domain' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => DataManagementSchema::DOMAIN],
+        'user' => ['type' => JsonRule::OBJECT_TYPE, 'schema' => DataManagementSchema::MINIMAL_USER]
     ];
 }
