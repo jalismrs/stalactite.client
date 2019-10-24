101e08cb383006a5d673f442b8f98ce339c85c68
hunomina <hugo.f972@gmail.com>
2019-10-24 14:59:50 +0200

    Add AuthTokenClient to AccessManagemet API client

--- src/AccessManagement/Client.php
+++ src/AccessManagement/Client.php
@@ -3,8 +3,22 @@
 namespace jalismrs\Stalactite\Client\AccessManagement;
 
 use jalismrs\Stalactite\Client\AbstractClient;
+use jalismrs\Stalactite\Client\AccessManagement\AuthToken\AuthTokenClient;
 
 class Client extends AbstractClient
 {
+    public const API_URL_PREFIX = '/access';
 
+    /**
+     * @return AuthTokenClient
+     */
+    public function authToken(): AuthTokenClient
+    {
+        if (!($this->authTokenClient instanceof AuthTokenClient)) {
+            $this->authTokenClient = new AuthTokenClient($this->apiHost, $this->userAgent);
+            $this->authTokenClient->setHttpClient($this->getHttpClient());
+        }
+
+        return $this->authTokenClient;
+    }
 }
