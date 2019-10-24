09334249ebf5a853b29010bab72a0eb6ae375f53
hunomina <hugo.f972@gmail.com>
2019-10-24 16:26:51 +0200

    Add UserClient to AccessManagement API client

--- src/AccessManagement/Client.php
+++ src/AccessManagement/Client.php
@@ -4,13 +4,35 @@ namespace jalismrs\Stalactite\Client\AccessManagement;
 
 use jalismrs\Stalactite\Client\AbstractClient;
 use jalismrs\Stalactite\Client\AccessManagement\AuthToken\AuthTokenClient;
+use jalismrs\Stalactite\Client\AccessManagement\Customer\CustomerClient;
+use jalismrs\Stalactite\Client\AccessManagement\User\UserClient;
 
 class Client extends AbstractClient
 {
     public const API_URL_PREFIX = '/access';
 
+    /** @var UserClient $userClient */
+    private $userClient;
+
     /** @var CustomerClient $customerClient */
     private $customerClient;
+
+    /** @var AuthTokenClient $authTokenClient */
+    private $authTokenClient;
+
+    /**
+    /**
+     * @return UserClient
+     */
+    public function users(): UserClient
+    {
+        if (!($this->userClient instanceof UserClient)) {
+            $this->userClient = new UserClient($this->apiHost, $this->userAgent);
+            $this->userClient->setHttpClient($this->getHttpClient());
+        }
+
+        return $this->userClient;
+    }
 
     /**
      * @return CustomerClient
