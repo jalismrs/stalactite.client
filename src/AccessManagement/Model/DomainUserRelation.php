8045babdd3604f4acf58ad36f65961a88073ebf1
hunomina <hugo.f972@gmail.com>
2019-10-24 15:33:39 +0200

    Add relations entity models

---
+++ src/AccessManagement/Model/DomainUserRelation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace jalismrs\Stalactite\Client\AccessManagement\Model;
+
+use jalismrs\Stalactite\Client\DataManagement\Model\User;
+
+class DomainUserRelation extends DomainRelation
+{
+    /** @var User|null $user */
+    private $user;
+
+    /**
+     * @return User|null
+     */
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User|null $user
+     * @return DomainUserRelation
+     */
+    public function setUser(?User $user): DomainUserRelation
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return array
+     * Return the object as an array
+     */
+    public function asArray(): array
+    {
+        return [
+            'uid' => $this->uid,
+            'domain' => $this->domain ? $this->domain->asArray() : null,
+            'user' => $this->user ? $this->user->asArray() : null
+        ];
+    }
+}
