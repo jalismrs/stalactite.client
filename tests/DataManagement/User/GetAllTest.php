9c27b410899d1d47062915b0f0cd0d4b6cffd9ca
kevin.castejon <kevin.castejon@jalis.fr>
2020-01-16 14:42:58 +0100

    split request tests into separate classes

---
+++ tests/DataManagement/User/GetAllTest.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jalismrs\Stalactite\Test\DataManagement\User;
+
+use hunomina\Validator\Json\Exception\InvalidDataTypeException;
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use Jalismrs\Stalactite\Client\ClientException;
+use Jalismrs\Stalactite\Client\DataManagement\Model\UserModel;
+use Jalismrs\Stalactite\Client\DataManagement\User\Client;
+use Jalismrs\Stalactite\Test\DataManagement\ModelFactory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+/**
+ * ClientTest
+ *
+ * @package Jalismrs\Stalactite\Test\DataManagement\UserModel
+ */
+class GetAllTest extends
+    TestCase
+{
+    /**
+     * testGetAll
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \hunomina\Validator\Json\Exception\InvalidDataTypeException
+     * @throws \hunomina\Validator\Json\Exception\InvalidSchemaException
+     * @throws \Jalismrs\Stalactite\Client\ClientException
+     */
+    public function testGetAll() : void
+    {
+        $mockHttpClient = new MockHttpClient(
+            [
+                new MockResponse(
+                    json_encode(
+                        [
+                            'success' => true,
+                            'error'   => null,
+                            'users'   => [
+                                ModelFactory::getTestableUser()
+                                            ->asMinimalArray()
+                            ]
+                        ],
+                        JSON_THROW_ON_ERROR
+                    )
+                )
+            ]
+        );
+        
+        $mockAPIClient = new Client(
+            'http://fakeClient',
+            null,
+            $mockHttpClient
+        );
+        
+        $response = $mockAPIClient->getAll(
+            'fake user jwt'
+        );
+        self::assertTrue($response->success());
+        self::assertNull($response->getError());
+        self::assertContainsOnlyInstancesOf(
+            UserModel::class,
+            $response->getData()['users']
+        );
+    }
+    
+    /**
+     * @throws ClientException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowOnInvalidResponseOnGetAll() : void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+        
+        $mockHttpClient = new MockHttpClient(
+            [
+                new MockResponse(
+                    json_encode(
+                        [
+                            'success' => true,
+                            'error'   => null,
+                            'users'   => null
+                            // invalid type
+                        ],
+                        JSON_THROW_ON_ERROR
+                    )
+                )
+            ]
+        );
+        
+        $mockAPIClient = new Client(
+            'http://fakeClient',
+            null,
+            $mockHttpClient
+        );
+        
+        $mockAPIClient->getAll(
+            'fake user jwt'
+        );
+    }
+}
---
+++ tests/DataManagement/User/PhoneLine/GetAllTest.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jalismrs\Stalactite\Test\DataManagement\User\PhoneLine;
+
+use hunomina\Validator\Json\Exception\InvalidDataTypeException;
+use hunomina\Validator\Json\Exception\InvalidSchemaException;
+use Jalismrs\Stalactite\Client\ClientException;
+use Jalismrs\Stalactite\Client\DataManagement\Model\PhoneLineModel;
+use Jalismrs\Stalactite\Client\DataManagement\User\PhoneLine\Client;
+use Jalismrs\Stalactite\Test\DataManagement\ModelFactory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+/**
+ * GetAllTest
+ *
+ * @package Jalismrs\Stalactite\Test\DataManagement\User\Post
+ */
+class GetAllTest extends
+    TestCase
+{
+    /**
+     * testGetAll
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \hunomina\Validator\Json\Exception\InvalidDataTypeException
+     * @throws \hunomina\Validator\Json\Exception\InvalidSchemaException
+     * @throws \Jalismrs\Stalactite\Client\ClientException
+     */
+    public function testGetAll() : void
+    {
+        $mockHttpClient = new MockHttpClient(
+            [
+                new MockResponse(
+                    json_encode(
+                        [
+                            'success'    => true,
+                            'error'      => null,
+                            'phoneLines' => [
+                                ModelFactory::getTestablePhoneLine()
+                                            ->asArray()
+                            ]
+                        ],
+                        JSON_THROW_ON_ERROR
+                    )
+                )
+            ]
+        );
+        
+        $mockAPIClient = new Client(
+            'http://fakeHost',
+            null,
+            $mockHttpClient
+        );
+        
+        $response = $mockAPIClient->getAll(
+            ModelFactory::getTestableUser(),
+            'fake user jwt'
+        );
+        self::assertTrue($response->success());
+        self::assertNull($response->getError());
+        self::assertContainsOnlyInstancesOf(
+            PhoneLineModel::class,
+            $response->getData()['phoneLines']
+        );
+    }
+    
+    /**
+     * @throws ClientException
+     * @throws InvalidDataTypeException
+     * @throws InvalidSchemaException
+     */
+    public function testThrowExceptionOnInvalidResponseGetAll() : void
+    {
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(ClientException::INVALID_API_RESPONSE_ERROR);
+        
+        $mockHttpClient = new MockHttpClient(
+            [
+                new MockResponse(
+                    json_encode(
+                        [
+                            'success'    => true,
+                            'error'      => null,
+                            'phoneLines' => ModelFactory::getTestablePhoneLine()
+                                                        ->asArray()
+                            // invalid type
+                        ],
+                        JSON_THROW_ON_ERROR
+                    )
+                )
+            ]
+        );
+        
+        $mockAPIClient = new Client(
+            'http://fakeHost',
+            null,
+            $mockHttpClient
+        );
+        
+        $mockAPIClient->getAll(
+            ModelFactory::getTestableUser(),
+            'fake user jwt'
+        );
+    }
+}
