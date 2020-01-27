a4fe71e865be9b8cd30c49c22938aebadf9184e1
kevin.castejon <kevin.castejon@jalis.fr>
2020-01-27 14:57:27 +0100

    add Post serialization tests

---
+++ tests/Data/Model/PostTest.php
@@ -0,0 +1,158 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jalismrs\Stalactite\Client\Tests\Data\Model;
+
+use Jalismrs\Stalactite\Client\Tests\Data\ModelFactory;
+use Jalismrs\Stalactite\Client\Util\Serializer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * PostTest
+ *
+ * @package Jalismrs\Stalactite\Client\Tests\Data\Model
+ */
+class PostTest extends
+    TestCase
+{
+    /**
+     * testGroupCommon
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\CircularReferenceException
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\LogicException
+     * @throws \Symfony\Component\Serializer\Exception\MappingException
+     */
+    public function testGroupCommon() : void
+    {
+        $serializer = Serializer::create();
+        
+        $model = ModelFactory::getTestablePost();
+        
+        $actual = $serializer->normalize($model);
+        
+        $expected = [];
+        
+        self::assertEqualsCanonicalizing($expected, $actual);
+    }
+    
+    /**
+     * testGroupMain
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\CircularReferenceException
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\LogicException
+     * @throws \Symfony\Component\Serializer\Exception\MappingException
+     */
+    public function testGroupMain() : void
+    {
+        $serializer = Serializer::create();
+        
+        $model = ModelFactory::getTestablePost();
+        
+        $actual = $serializer->normalize(
+            $model,
+            [
+                'groups' => [
+                    'main',
+                ],
+            ]
+        );
+        
+        $expected = [
+            'uid'         => $model->getUid(),
+            'name'        => $model->getName(),
+            'shortName'   => $model->getShortName(),
+            'adminAccess' => $model->hasAdminAccess(),
+            'allowAccess' => $model->allowAccess(),
+        ];
+        
+        self::assertEqualsCanonicalizing($expected, $actual);
+    }
+    
+    /**
+     * testGroupCreate
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\CircularReferenceException
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\LogicException
+     * @throws \Symfony\Component\Serializer\Exception\MappingException
+     */
+    public function testGroupCreate() : void
+    {
+        $serializer = Serializer::create();
+        
+        $model = ModelFactory::getTestablePost();
+        
+        $actual = $serializer->normalize(
+            $model,
+            [
+                'groups' => [
+                    'create',
+                ],
+            ]
+        );
+        
+        $expected = [
+            'access'    => $model->allowAccess(),
+            'admin'     => $model->hasAdminAccess(),
+            'name'      => $model->getName(),
+            'shortName' => $model->getShortName(),
+        ];
+        
+        self::assertEqualsCanonicalizing($expected, $actual);
+    }
+    
+    /**
+     * testGroupUpdate
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\CircularReferenceException
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\LogicException
+     * @throws \Symfony\Component\Serializer\Exception\MappingException
+     */
+    public function testGroupUpdate() : void
+    {
+        $serializer = Serializer::create();
+        
+        $model = ModelFactory::getTestablePost();
+        
+        $actual = $serializer->normalize(
+            $model,
+            [
+                'groups' => [
+                    'update',
+                ],
+            ]
+        );
+        
+        $expected = [
+            'access'    => $model->allowAccess(),
+            'admin'     => $model->hasAdminAccess(),
+            'name'      => $model->getName(),
+            'shortName' => $model->getShortName(),
+        ];
+        
+        self::assertEqualsCanonicalizing($expected, $actual);
+    }
+}
