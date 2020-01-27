7e06b780bd911105eaafe323e2954a5ea2cce3ea
kevin.castejon <kevin.castejon@jalis.fr>
2020-01-27 14:57:50 +0100

    add DomainCustomerRelation serialization tests

---
+++ tests/Access/Model/DomainCustomerRelationTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jalismrs\Stalactite\Client\Tests\Access\Model;
+
+use Jalismrs\Stalactite\Client\Tests\Access\ModelFactory;
+use Jalismrs\Stalactite\Client\Util\Serializer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * DomainUserRelationTest
+ *
+ * @package Jalismrs\Stalactite\Client\Tests\Access\Model
+ */
+class DomainCustomerRelationTest extends
+    TestCase
+{
+    /**
+     * testGroupCommon
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\CircularReferenceException
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\LogicException
+     * @throws \Symfony\Component\Serializer\Exception\MappingException
+     */
+    public function testGroupCommon() : void
+    {
+        $serializer = Serializer::create();
+        
+        $model = ModelFactory::getTestableDomainCustomerRelation();
+        
+        $actual = $serializer->normalize($model);
+        
+        $expected = [];
+        
+        self::assertEqualsCanonicalizing($expected, $actual);
+    }
+    
+    /**
+     * testGroupMain
+     *
+     * @return void
+     *
+     * @throws \PHPUnit\Framework\ExpectationFailedException
+     * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\CircularReferenceException
+     * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
+     * @throws \Symfony\Component\Serializer\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Serializer\Exception\LogicException
+     * @throws \Symfony\Component\Serializer\Exception\MappingException
+     */
+    public function testGroupMain() : void
+    {
+        $serializer = Serializer::create();
+    
+        $model = ModelFactory::getTestableDomainCustomerRelation();
+        
+        $actual = $serializer->normalize(
+            $model,
+            [
+                'groups' => [
+                    'main',
+                ],
+            ]
+        );
+        
+        $expected = [
+            'uid'    => $model->getUid(),
+            'domain' => $serializer->normalize(
+                $model->getDomain(),
+                [
+                    'groups' => [
+                        'main',
+                    ],
+                ]
+            ),
+            'customer'   => $serializer->normalize(
+                $model->getCustomer(),
+                [
+                    'groups' => [
+                        'main',
+                    ],
+                ]
+            ),
+        ];
+        
+        self::assertEqualsCanonicalizing($expected, $actual);
+    }
+}
