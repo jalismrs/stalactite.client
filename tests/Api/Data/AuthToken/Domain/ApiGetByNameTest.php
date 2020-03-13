f2236129c49d7d2a40daaa5f16a5cd5a7568e6fa
hunomina <hugo.f972@gmail.com>
2020-03-13 15:15:21 +0100

    Refactor Data\AuthToken\Domain\Service to use the new Client

--- tests/Api/Data/AuthToken/Domain/ApiGetByNameTest.php
+++ tests/Api/Data/AuthToken/Domain/ApiGetByNameTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Jalismrs\Stalactite\Client\Tests\Api\Data\AuthToken\Domain;
 
@@ -7,16 +7,11 @@ use Jalismrs\Stalactite\Client\Client;
 use Jalismrs\Stalactite\Client\Data\AuthToken\Domain\Service;
 use Jalismrs\Stalactite\Client\Data\Model\Domain;
 use Jalismrs\Stalactite\Client\Exception\ClientException;
-use Jalismrs\Stalactite\Client\Exception\RequestException;
 use Jalismrs\Stalactite\Client\Exception\SerializerException;
-use Jalismrs\Stalactite\Client\Exception\ValidatorException;
 use Jalismrs\Stalactite\Client\Tests\Api\EndpointTest;
 use Jalismrs\Stalactite\Client\Tests\Data\ModelFactory;
 use Jalismrs\Stalactite\Client\Tests\MockHttpClientFactory;
 use Jalismrs\Stalactite\Client\Util\Serializer;
-use PHPUnit\Framework\ExpectationFailedException;
-use PHPUnit\Framework\MockObject\RuntimeException;
-use SebastianBergmann\RecursionContext\InvalidArgumentException;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 /**
@@ -24,80 +19,41 @@ use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
  *
  * @package Jalismrs\Stalactite\Client\Tests\Api\Data\AuthToken\Domain
  */
-class ApiGetByNameTest extends
-    EndpointTest
+class ApiGetByNameTest extends EndpointTest
 {
     /**
-     * testGetByName
-     *
-     * @return void
-     *
      * @throws ClientException
-     * @throws ExpectationFailedException
-     * @throws InvalidArgumentException
-     * @throws RequestException
      * @throws SerializerException
-     * @throws ValidatorException
      */
-    public function testGetByName() : void
+    public function testGetByName(): void
     {
-        $mockClient  = new Client('http://fakeHost');
+        $mockClient = new Client('http://fakeHost');
         $mockService = new Service($mockClient);
         $mockClient->setHttpClient(
             MockHttpClientFactory::create(
-                json_encode(
-                    [
-                        'success' => true,
-                        'error'   => null,
-                        'domains' => [
-                            Serializer::getInstance()
-                                      ->normalize(
-                                          ModelFactory::getTestableDomain(),
-                                          [
-                                              AbstractNormalizer::GROUPS => [
-                                                  'main',
-                                              ],
-                                          ]
-                                      )
-                        ]
-                    ],
-                    JSON_THROW_ON_ERROR
-                )
+                json_encode([
+                    Serializer::getInstance()
+                        ->normalize(
+                            ModelFactory::getTestableDomain(),
+                            [
+                                AbstractNormalizer::GROUPS => ['main']
+                            ]
+                        )
+                ], JSON_THROW_ON_ERROR)
             )
         );
-        
-        $response = $mockService->getByName(
-            ModelFactory::getTestableDomain()
-                        ->getName(),
-            'fake API auth token'
-        );
-        self::assertTrue($response->isSuccess());
-        self::assertNull($response->getError());
-        self::assertContainsOnlyInstancesOf(
-            Domain::class,
-            $response->getData()['domains']
-        );
+
+        $response = $mockService->getByName(ModelFactory::getTestableDomain()->getName(), 'fake API auth token');
+
+        self::assertContainsOnlyInstancesOf(Domain::class, $response->getBody());
     }
-    
+
     /**
-     * testRequestMethodCalledOnce
-     *
-     * @return void
-     *
      * @throws ClientException
-     * @throws RequestException
-     * @throws RuntimeException
-     * @throws SerializerException
-     * @throws ValidatorException
      */
-    public function testRequestMethodCalledOnce() : void
+    public function testRequestMethodCalledOnce(): void
     {
         $mockService = new Service($this->createMockClient());
-    
-        $mockService->getByName(
-            ModelFactory::getTestableDomain()
-                        ->getName(),
-            'fake API auth token'
-        );
+        $mockService->getByName(ModelFactory::getTestableDomain()->getName(), 'fake API auth token');
     }
 }
