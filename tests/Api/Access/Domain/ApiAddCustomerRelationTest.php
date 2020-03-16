267aa0481dd5b63b5c5440180eba3dc498dfb771
hunomina <hugo.f972@gmail.com>
2020-03-16 13:19:37 +0100

    Refactor Access\Domain\Service to use the new Client

--- tests/Api/Access/Domain/ApiAddCustomerRelationTest.php
+++ tests/Api/Access/Domain/ApiAddCustomerRelationTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Jalismrs\Stalactite\Client\Tests\Api\Access\Domain;
 
@@ -7,19 +7,13 @@ use Jalismrs\Stalactite\Client\Access\Domain\Service;
 use Jalismrs\Stalactite\Client\Access\Model\DomainCustomerRelation;
 use Jalismrs\Stalactite\Client\Client;
 use Jalismrs\Stalactite\Client\Exception\ClientException;
-use Jalismrs\Stalactite\Client\Exception\RequestException;
 use Jalismrs\Stalactite\Client\Exception\SerializerException;
-use Jalismrs\Stalactite\Client\Exception\ServiceException;
-use Jalismrs\Stalactite\Client\Exception\ValidatorException;
+use Jalismrs\Stalactite\Client\Exception\Service\AccessServiceException;
 use Jalismrs\Stalactite\Client\Tests\Access\ModelFactory;
 use Jalismrs\Stalactite\Client\Tests\Api\EndpointTest;
 use Jalismrs\Stalactite\Client\Tests\Data\ModelFactory as DataTestModelFactory;
 use Jalismrs\Stalactite\Client\Tests\MockHttpClientFactory;
 use Jalismrs\Stalactite\Client\Util\Serializer;
-use PHPUnit\Framework\Exception;
-use PHPUnit\Framework\ExpectationFailedException;
-use PHPUnit\Framework\MockObject\RuntimeException;
-use SebastianBergmann\RecursionContext\InvalidArgumentException;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 /**
@@ -27,127 +21,83 @@ use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
  *
  * @package Jalismrs\Stalactite\Client\Tests\Api\Access\Domain
  */
-class ApiAddCustomerRelationTest extends
-    EndpointTest
+class ApiAddCustomerRelationTest extends EndpointTest
 {
     /**
-     * testAddCustomerRelation
-     *
-     * @return void
-     *
      * @throws ClientException
-     * @throws Exception
-     * @throws ExpectationFailedException
-     * @throws InvalidArgumentException
-     * @throws RequestException
      * @throws SerializerException
-     * @throws ServiceException
-     * @throws ValidatorException
      */
-    public function testAddCustomerRelation() : void
+    public function testAddCustomerRelation(): void
     {
-        $mockClient  = new Client('http://fakeHost');
+        $mockClient = new Client('http://fakeHost');
         $mockService = new Service($mockClient);
         $mockClient->setHttpClient(
             MockHttpClientFactory::create(
                 json_encode(
-                    [
-                        'success'  => true,
-                        'error'    => null,
-                        'relation' => Serializer::getInstance()
-                                                ->normalize(
-                                                    ModelFactory::getTestableDomainCustomerRelation(),
-                                                    [
-                                                        AbstractNormalizer::GROUPS => [
-                                                            'main',
-                                                        ],
-                                                    ]
-                                                )
-                    ],
+                    Serializer::getInstance()
+                        ->normalize(
+                            ModelFactory::getTestableDomainCustomerRelation(),
+                            [
+                                AbstractNormalizer::GROUPS => ['main']
+                            ]
+                        ),
                     JSON_THROW_ON_ERROR
                 )
             )
         );
-        
+
         $response = $mockService->addCustomerRelation(
             DataTestModelFactory::getTestableDomain(),
             DataTestModelFactory::getTestableCustomer(),
             'fake user jwt'
         );
-        
-        static::assertTrue($response->isSuccess());
-        static::assertNull($response->getError());
-        static::assertInstanceOf(DomainCustomerRelation::class, $response->getData()['relation']);
+
+        static::assertInstanceOf(DomainCustomerRelation::class, $response->getBody());
     }
-    
+
     /**
-     * testThrowDomainLacksUid
-     *
-     * @return void
-     *
      * @throws ClientException
-     * @throws RequestException
-     * @throws SerializerException
-     * @throws ServiceException
-     * @throws ValidatorException
      */
-    public function testThrowDomainLacksUid() : void
+    public function testThrowDomainLacksUid(): void
     {
-        $this->expectException(ServiceException::class);
-        $this->expectExceptionMessage('Domain lacks a uid');
-        
-        $mockClient  = new Client('http://fakeHost');
+        $this->expectException(AccessServiceException::class);
+        $this->expectExceptionCode(AccessServiceException::MISSING_DOMAIN_UID);
+
+        $mockClient = new Client('http://fakeHost');
         $mockService = new Service($mockClient);
-        
+
         $mockService->addCustomerRelation(
             DataTestModelFactory::getTestableDomain()->setUid(null),
             DataTestModelFactory::getTestableCustomer(),
             'fake user jwt'
         );
     }
-    
+
     /**
-     * testThrowCustomerLacksUid
-     *
-     * @return void
-     *
      * @throws ClientException
-     * @throws RequestException
-     * @throws SerializerException
-     * @throws ServiceException
-     * @throws ValidatorException
      */
-    public function testThrowCustomerLacksUid() : void
+    public function testThrowCustomerLacksUid(): void
     {
-        $this->expectException(ServiceException::class);
-        $this->expectExceptionMessage('Customer lacks a uid');
-        
-        $mockClient  = new Client('http://fakeHost');
+        $this->expectException(AccessServiceException::class);
+        $this->expectExceptionCode(AccessServiceException::MISSING_CUSTOMER_UID);
+
+        $mockClient = new Client('http://fakeHost');
         $mockService = new Service($mockClient);
-        
+
         $mockService->addCustomerRelation(
             DataTestModelFactory::getTestableDomain(),
             DataTestModelFactory::getTestableCustomer()->setUid(null),
             'fake user jwt'
         );
     }
-    
+
     /**
-     * testRequestMethodCalledOnce
-     *
-     * @return void
-     *
      * @throws ClientException
-     * @throws RequestException
-     * @throws RuntimeException
-     * @throws SerializerException
-     * @throws ServiceException
-     * @throws ValidatorException
      */
-    public function testRequestMethodCalledOnce() : void
+    public function testRequestMethodCalledOnce(): void
     {
         $mockService = new Service($this->createMockClient());
-    
+
         $mockService->addCustomerRelation(
             DataTestModelFactory::getTestableDomain(),
             DataTestModelFactory::getTestableCustomer(),
